bcd34c0f725b053292854171227dcd9ece5c1bb5
tannyque <tannyque1992@gmail.com>
2018-09-01 19:20:03 +0100

    Populates dropdown and finds selected index :bulb:

---
+++ src/views/select_view.js
@@ -0,0 +1,32 @@
+const PubSub = require('../helpers/pub_sub.js');
+
+const SelectView = function (selectElement) {
+  this.selectElement = selectElement;
+};
+
+SelectView.prototype.bindEvents = function () {
+  PubSub.subscribe('Amiibos:amiibos-series-ready', (event) => {
+    this.populateSelect(event.detail)
+  });
+
+  this.selectElement.addEventListener('change', (event) => {
+    const selectedIndex = event.target.value;
+    PubSub.publish('SelectView:change', selectedIndex);
+  });
+};
+
+SelectView.prototype.populateSelect = function (series) {
+  series.forEach((series, index) => {
+    const option = this.createSeriesOption(series, index);
+    this.selectElement.appendChild(option);
+  });
+};
+
+SelectView.prototype.createSeriesOption = function (series, index) {
+  const option = document.createElement('option');
+  option.textContent = series;
+  option.value = index;
+  return option;
+};
+
+module.exports = SelectView;

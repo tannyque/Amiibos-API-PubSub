587c69bc10a41190a102041014cafed0aa68c6df
tannyque <tannyque1992@gmail.com>
2018-09-01 10:20:25 +0100

    Adds list view :pencil:

--- src/app.js
+++ src/app.js
@@ -1,6 +1,11 @@
 const Amiibos = require('./models/amiibos.js');
+const AmiibosListView = require('./views/amiibos_list_view');
 
 document.addEventListener('DOMContentLoaded', () => {
+  const amiibosListContainer = document.querySelector('#amiibos')
+  const amiibosListView = new AmiibosListView(amiibosListContainer);
+  amiibosListView.bindEvents();
+
   const amiibos = new Amiibos("http://www.amiiboapi.com/api/amiibo/");
   amiibos.getData();
 })

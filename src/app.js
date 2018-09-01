97bdb1556b3c34bc6b8a632c7df2e49333bb9555
tannyque <tannyque1992@gmail.com>
2018-09-01 19:20:57 +0100

    Adds select view to app and populates dropdown :ok_hand:

--- src/app.js
+++ src/app.js
@@ -1,8 +1,13 @@
 const Amiibos = require('./models/amiibos.js');
 const AmiibosListView = require('./views/amiibos_list_view');
+const SelectView = require('./views/select_view.js');
 // const AmiiboView = require('./views/amiibo_view.js')
 
 document.addEventListener('DOMContentLoaded', () => {
+  const selectElement = document.querySelector('select#amiibo-select')
+  const selectView = new SelectView(selectElement);
+  selectView.bindEvents();
+
   const amiibosListContainer = document.querySelector('#amiibos')
   const amiibosListView = new AmiibosListView(amiibosListContainer);
   amiibosListView.bindEvents();

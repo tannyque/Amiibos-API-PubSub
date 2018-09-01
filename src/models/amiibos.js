70b5cd5d988b0c4f9f2877b22a6425f932a35aa9
tannyque <tannyque1992@gmail.com>
2018-09-01 10:09:18 +0100

    Changes publishing data to amiibos :fire:

--- src/models/amiibos.js
+++ src/models/amiibos.js
@@ -18,7 +18,7 @@ Amiibos.prototype.getData = function () {
 
 Amiibos.prototype.handleData = function (data) {
   this.amiibos = data.amiibo;
-  PubSub.publish("Amiibos:amiibos-data-ready", data);
+  PubSub.publish("Amiibos:amiibos-data-ready", this.amiibos);
 };
 
 module.exports = Amiibos;
